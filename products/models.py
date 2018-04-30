c7cc6d17d399b04144250033d1fe9a500ca02f78
Ari Aalto <ari.h.aalto@gmail.com>
2018-04-30 08:34:30 +0300

    Product Hunter 0.1

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    title   = models.CharField(max_length=255)
+    url     = models.URLField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    votes_total = models.IntegerField(default=1)
+    image   = models.ImageField(blank=True, null=True, upload_to='images/')
+    icon    = models.ImageField(blank=True, null=True, upload_to='icons/')
+    body    = models.TextField(max_length=1000)
+    hunter  = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%Y-%m-%d, %H:%M')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:220]
--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import home
+from .views import home, create_product, detail_product, vote_product
 
 urlpatterns = [
-    path('', home, name='home_home')
+    path('', home, name='home_home'),
+    path('create/', create_product, name='product_create'),
+    path('<int:product_id>', detail_product, name='product_detail'),
+    path('<int:product_id>/vote_product', vote_product, name='upvote'),
 ]
